968e866d088a2005c6bcfd0603aedb2081f588c5
Paweł Kuriata <p.kuriata@sente.pl>
2019-06-09 18:21:05 +0200

    Dodanie kontrolera z meczami

---
+++ DALK.PL ANALYZER/Controllers/MatchesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DALK.PL_ANALYZER.Controllers
+{
+    public class MatchesController : Controller
+    {
+        // GET: Matches
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
