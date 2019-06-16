1a91c58d7b3c31bb1961946514a00ba23ad863d2
Paweł Kuriata <p.kuriata@sente.pl>
2019-06-17 01:45:49 +0200

    1. Another gitignore modifiaction. 2. Set right heigh for element in match

---
+++ DALK.PL_ANALYZER/Scripts/modernizr-site.js
@@ -0,0 +1,19 @@
+﻿function setMaxHeightForAllElementsInClass(className) {
+    var maxCallback = (max, cur) => Math.max(max, cur);
+
+    var allElements = document.querySelectorAll(className);
+    if (allElements.length > 0)
+    {
+        var elementsHeights = Array.from(allElements, m => m.offsetHeight);
+        var hMax = elementsHeights.reduce(maxCallback);
+        Array.from(allElements).forEach(function (el) {
+            el.style.height = hMax + "px";
+        });
+    }
+}
+
+function setHeightForMvpAndDesc() {
+    setMaxHeightForAllElementsInClass(".match__mvp");
+    setMaxHeightForAllElementsInClass(".match__description");
+}
+
