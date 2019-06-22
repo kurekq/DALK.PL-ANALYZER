e4153beede5ae448858b72d49b7921b7bd89e0fc
Paweł Kuriata <p.kuriata@sente.pl>
2019-06-22 15:28:00 +0200

    Installing bootstrap. Change source to partials and files (images for example).

--- DALK.PL_ANALYZER/Controllers/HomeController.cs
+++ DALK.PL_ANALYZER/Controllers/HomeController.cs
@@ -26,5 +26,45 @@ namespace DALK.PL_ANALYZER.Controllers
 
             return View();
         }
+
+        public PartialViewResult _LeftSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _DetachedLeftSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _Footer()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _HorizontalNav()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _LeftSideBarLight()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _RightSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _TopBarDark()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _TopBar()
+        {
+            return PartialView();
+        }
     }
 }
